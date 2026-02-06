bb3da7efe07312cf6807f94014bf97781ce5f24f
신성민 <sseongmin0114-pixel>
2026-02-06 10:34:54 +0900

    업데이트

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,8 @@
 import { createRoot } from "react-dom/client";
 
-import App from "./App.jsx";
-import Home from "./Home.jsx";
-import "./Home.css";
 const root = document.querySelector("#mango");
 createRoot(mango).render(
-  <div>
-    <Home />;
-    <Home />;
-    <Home />;
-    <Home />;
-    <Home />;
-    <Home />;
-  </div>,
+  
 );
 
 // react에서 대문자로 시작하는 식별자는 -> 컴포넌트
